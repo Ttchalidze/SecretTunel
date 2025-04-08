60a232d4119f92f79f618e05bb40ce69d3ac7a7e
tornike thcalidze <ttchalidze@gmail.com>
2025-04-08 00:31:54 -0400

    first1

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -8,11 +8,34 @@ export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+  const signup = async (Credential) => {
+    try {
+      const response = await fetch(API + "/singup", {
+        method: "post",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(Credentials),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const auth = async () => {
+    try {
+      if (!token) throw Error("no token found");
+      const response = await fetch(API + "/authenticate", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) throw Error("authentication fail");
+      setLocation("TUNNEL");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  // TODO: authenticate
-
-  const value = { location };
+  const value = { signup, auth, location };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
